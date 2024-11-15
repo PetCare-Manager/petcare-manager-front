0b9ad26e0024704df5d9ead59f9642773a28ccb5
Carolina Romero Cotán <114145394+Carol-88@users.noreply.github.com>
2024-11-15 13:31:14 +0100

    Revert "Feature/35 user avatar"

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve("react-native-svg-transformer"),
-};
-
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
-  sourceExts: [...config.resolver.sourceExts, "svg"],
-};
-
-module.exports = withNativeWind(config, { input: './assets/css/_tailwind.css' });
+module.exports = withNativeWind(config, { input: './assets/css/_tailwind.css' })
