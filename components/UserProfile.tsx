054874151cd8b3457eae6f11820468b7942a94a8
Carol-88 <gralleta@gmail.com>
2024-11-15 13:38:07 +0100

    update

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { AddPetCard } from "./AddPetCard";
+import { UserAvatar } from "./UserAvatar";
 
 type UserProfileProps = NativeStackScreenProps<RootStackParamList, "PetForm">;
 
@@ -13,6 +14,7 @@ type RootStackParamList = {
 export const UserProfile: React.FC<UserProfileProps> = ({ navigation }) => {
   return (
     <View className="flex items-center justify-center bg-white w-full h-full p-4">
+      <UserAvatar />
       <Text className="mt-4 text-2xl font-raleway-regular text-gray-800">
         Hola, ¡disfruta tu día!
       </Text>
