d30442b71822c6489ba0cb9df77bcabca2cc8931
DarelNC <twh.darelnc@hotmail.com>
2024-11-15 20:49:25 +0100

    Rebase with develop to fix timeline

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,28 +1,23 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
 import { UserAvatar } from "./UserAvatar";
 
 export interface UserProfileProps {
-  userName: string;
+  name: string; // Nombre del usuario
+  imageUrl?: string; // Imagen inicial del usuario
 }
 
-export const UserProfile: React.FC<UserProfileProps> = ({ userName }) => {
+export const UserProfile: React.FC<UserProfileProps> = ({ name, imageUrl }) => {
   return (
     <View className="flex items-center justify-center bg-white w-full h-full p-4">
-      {/* Contenedor del avatar y el ícono de edición */}
-      <View className="relative items-center">
-        <UserAvatar initials={userName.charAt(0).toUpperCase()} />
-
-        {/* Ícono de editar perfil */}
-        <TouchableOpacity className="absolute top-2 right-2 bg-white rounded-full p-1">
-          <MaterialIcons name="edit" size={20} color="#3b3b58" />
-        </TouchableOpacity>
-      </View>
+      {/* Avatar del usuario */}
+      <UserAvatar
+        imageUrl={imageUrl} // Imagen inicial (si existe)
+      />
 
       {/* Mensaje de bienvenida */}
-      <Text className="mt-4 text-2xl font-semibold text-gray-800">
-        Hola <Text className="font-bold">{userName}</Text>, ¡disfruta tu día!
+      <Text className="mt-4 text-2xl font-raleway-regular text-gray-800">
+        Hola <Text className="font-bold">{name}</Text>, ¡disfruta tu día!
       </Text>
     </View>
   );
