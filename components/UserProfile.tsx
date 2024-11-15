323b8ad18af67870a37e22f0de5217ae3975f0b3
Carol-88 <gralleta@gmail.com>
2024-11-15 12:44:55 +0100

    create component addpetcard and change primary color

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { UserAvatar } from "./UserAvatar";
+import { AddPetCard } from "./AddPetCard";
 
 export interface UserProfileProps {
   name: string; // Nombre del usuario
@@ -8,6 +9,10 @@ export interface UserProfileProps {
 }
 
 export const UserProfile: React.FC<UserProfileProps> = ({ name, imageUrl }) => {
+  function handleAddPet(): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <View className="flex items-center justify-center bg-white w-full h-full p-4">
       {/* Avatar del usuario */}
@@ -19,6 +24,15 @@ export const UserProfile: React.FC<UserProfileProps> = ({ name, imageUrl }) => {
       <Text className="mt-4 text-2xl font-raleway-regular text-gray-800">
         Hola <Text className="font-bold">{name}</Text>, ¡disfruta tu día!
       </Text>
+      {/* Sección de mascotas */}
+
+      <View className="w-full mt-6 px-4">
+        <Text className="text-lg font-afacad-semibold text-typography_2">
+          Mis mascotas
+        </Text>
+        {/* Botón Añadir Mascota */}
+        <AddPetCard onAddPet={handleAddPet} />
+      </View>
     </View>
   );
 };
