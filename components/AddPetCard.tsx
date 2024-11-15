bc40c9284c889fe2f9311410e128f73bab659949
Carol-88 <gralleta@gmail.com>
2024-11-15 13:28:34 +0100

    change petform, add navigation, add componet addpetcard

--- components/AddPetCard.tsx
+++ components/AddPetCard.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import { SvgIconsComponent } from "@/components/SvgIconsComponent";
 import { MaterialIcons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-export const AddPetCard: React.FC<{ onAddPet: () => void }> = ({
-  onAddPet,
-}) => {
+type RootStackParamList = {
+  PetForm: undefined;
+};
+// Definir el tipo de navegación para AddPetCard
+type AddPetCardProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList, "PetForm">;
+};
+
+export const AddPetCard: React.FC<AddPetCardProps> = ({ navigation }) => {
   return (
-    <View
-      className="flex flex-row items-center bg-white rounded-lg p-4"
-      style={{
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 3,
-      }}
-    >
+    <View className="flex flex-row items-center bg-white rounded-lg p-4">
       <View className="container flex">
         <Text className="text-typography mb-4 text-base font-raleway-light">
           Actualmente no dispones de mascotas en la app
@@ -25,7 +22,7 @@ export const AddPetCard: React.FC<{ onAddPet: () => void }> = ({
         <View className="flex-row justify-center items-center rounded-lg shadow p-4">
           {/* Icono de mascota */}
           <View className="flex bg-primary rounded-lg w-12 h-12 items-center justify-center mr-4">
-            <SvgIconsComponent type="logo1" containerClass="w-8 h-8" />
+            <MaterialIcons name="pets" size={24} color="#fff" />
           </View>
 
           {/* Texto de añadir mascota */}
@@ -33,7 +30,7 @@ export const AddPetCard: React.FC<{ onAddPet: () => void }> = ({
 
           {/* Botón de añadir mascota */}
           <TouchableOpacity
-            onPress={onAddPet}
+            onPress={() => navigation.navigate("PetForm")}
             className="bg-pink-200 rounded-full p-2"
           >
             <MaterialIcons name="add" size={24} color="#c04b71" />
