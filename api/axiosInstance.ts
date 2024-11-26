41fe1efb72b014ce3820c9420cdfe36df67587cb
Diego Guaman <diegoguaman05@hotmail.com>
2024-11-26 22:11:07 +0100

    Add axiosInstance and errorHanlder to manage the responde of back

---
+++ api/axiosInstance.ts
@@ -0,0 +1,23 @@
+// src/api/axiosInstance.ts
+
+import axios from "axios";
+import { handleBackendError } from "../utils/errorHandler";
+
+
+const axiosInstance = axios.create({
+  baseURL: process.env.BACK_URL, // Asegúrate de tener la URL en tu .env
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Interceptor de respuestas para manejar errores globalmente
+axiosInstance.interceptors.response.use(
+  (response) => response, // Si la respuesta es exitosa, la pasamos al siguiente paso.
+  (error) => {
+    // Aquí manejamos los errores con el errorHandler
+    return Promise.reject(handleBackendError(error)); // Llama al handler y lo pasa a donde se necesite
+  }
+);
+
+export default axiosInstance;
