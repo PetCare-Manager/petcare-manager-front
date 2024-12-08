cfe092e7f804f34b1d0b34ba23ae31ae85a4b46c
Carol-88 <gralleta@gmail.com>
2024-12-08 14:40:46 +0100

    add emailtaken function and logic, working in validate errors in emailregister

--- components/EmailRegister.tsx
+++ components/EmailRegister.tsx
@@ -9,11 +9,9 @@ import {
   ScrollView,
 } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Bubble } from "./Bubbles";
-import { SvgIconsComponent } from "./SvgIconsComponent";
-// Función de validación de email
-import { validateEmail } from "@/utils/validation";
-import userService from "@/services/userService";
+import { MaterialIcons } from "@expo/vector-icons";
+import { validateEmail } from "@/utils/validation"; // Asegúrate de tener esta función de validación
+import { userService } from "@/api/authApi"; // Tu servicio para registrar al usuario
 
 type RootStackParamList = {
   Home: undefined;
@@ -33,145 +31,278 @@ export const EmailRegister: React.FC<EmailRegisterProps> = ({ navigation }) => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [secureText, setSecureText] = useState(true);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [confirmPasswordValid, setConfirmPasswordValid] = useState(false);
 
-  // Validación de entrada de datos y mensaje de error
+  // Expresión regular para validar la contraseña
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
   const handleRegister = async () => {
+    setEmailError("");
+    setPasswordError("");
+    setConfirmPasswordError("");
+    setErrorMessage("");
+
+    // Verificar si el email ya está registrado
+    const emailTaken = await userService.isEmailTaken(email);
+    if (emailTaken) {
+      setEmailError("Este correo electrónico ya está registrado.");
+      return;
+    }
+
     if (!validateEmail(email)) {
-      setErrorMessage("Por favor, introduce un correo electrónico válido.");
+      setEmailError("Por favor, introduce un correo electrónico válido.");
       return;
     }
 
     if (password.length < 8) {
-      setErrorMessage("La contraseña debe tener al menos 8 caracteres.");
+      setPasswordError("La contraseña debe tener al menos 8 caracteres.");
+      return;
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      // Verificación de mayúsculas
+      setPasswordError(
+        "La contraseña debe contener al menos una letra mayúscula."
+      );
+      return;
+    }
+
+    if (!/\d/.test(password)) {
+      // Verificación de número
+      setPasswordError("La contraseña debe contener al menos un número.");
+      return;
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      // Verificación de símbolo
+      setPasswordError("La contraseña debe contener al menos un símbolo.");
       return;
     }
 
     if (password !== confirmPassword) {
-      setErrorMessage("Las contraseñas no coinciden.");
+      setConfirmPasswordError("Las contraseñas no coinciden.");
       return;
     }
 
-    // Si todas las validaciones pasan
-    setErrorMessage(""); // Limpiar mensaje de error si todo está correcto
-
-    // logica de peticiones al back
     try {
-      // Registra el usuario en el backend
       await userService.register(email, password);
-      //Navega al login si todo es correcto
       navigation.navigate("Login");
     } catch (error: any) {
-      setErrorMessage(error.message || "Error en el registro");
+      setErrorMessage(error.message || "Error en el registro.");
     }
   };
 
+  // Limpieza de errores cuando el valor del campo cambia
+  const handleEmailChange = (newEmail: string) => {
+    setEmail(newEmail);
+    if (emailError) {
+      setEmailError(""); // Limpia el error si se cambia el valor del email
+    }
+  };
+
+  const handlePasswordChange = (newPassword: string) => {
+    setPassword(newPassword);
+    if (passwordError) {
+      setPasswordError(""); // Limpia el error si se cambia el valor de la contraseña
+    }
+  };
+
+  const handleConfirmPasswordChange = (newConfirmPassword: string) => {
+    setConfirmPassword(newConfirmPassword);
+    if (confirmPasswordError) {
+      setConfirmPasswordError(""); // Limpia el error si se cambia el valor de la confirmación de contraseña
+    }
+  };
+
+  const isButtonDisabled = Boolean(
+    !email ||
+      !password ||
+      !confirmPassword ||
+      emailError ||
+      passwordError ||
+      confirmPasswordError
+  );
+
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <KeyboardAvoidingView behavior="padding" className="flex-1">
+    <SafeAreaView className="flex justify-between h-full">
+      <KeyboardAvoidingView
+        behavior="padding"
+        className="flex-1 justify-between"
+      >
         <ScrollView
           contentContainerStyle={{
             flexGrow: 1,
-            justifyContent: "center",
-            alignItems: "center", // Centra el contenido horizontalmente
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
           showsVerticalScrollIndicator={false}
         >
-          <SvgIconsComponent
-            containerClass="w-56 h-44 z-10 mt-[120px]"
-            type="logo1"
-          />
-
-          {/* Burbujas decorativas */}
-          <Bubble
-            containerClass="absolute -bottom-20 -right-16 z-0"
-            type="bubble1"
-            rotation={-45}
-          />
-          <Bubble
-            containerClass="absolute -top-20 -left-16 z-0"
-            type="bubble2"
-            rotation={-45}
-          />
-          <Bubble
-            containerClass="absolute top-36 right-10 z-0"
-            type="bubble3"
-            rotation={-45}
-          />
-          <Bubble
-            containerClass="absolute bottom-96 right-72 z-0"
-            type="bubble4"
-            rotation={-45}
-          />
-          <Bubble
-            containerClass="absolute bottom-32 right-80 z-0"
-            type="bubble5"
-            rotation={-60}
-          />
-
           <View className="flex gap-4 mt-4 mb-6 w-full px-12">
+            <Text className="font-raleway-semibold text-base">
+              Todo lo que necesitas de tu mascota, en tu bolsillo, a un solo
+              click.
+            </Text>
+
             {/* Correo electrónico */}
-            <Text className="-mb-3 uppercase font-afacad-regular text-base">
+            <Text className="-mb-3 text-typography_2 font-raleway-medium text-base">
               Correo electrónico
             </Text>
             <TextInput
-              placeholder="example@example.com"
+              placeholder="Introduce tu email"
               value={email}
               onChangeText={setEmail}
-              className="bg-[#f2f2f2] border border-inputborder rounded-lg w-full p-2"
+              className={`bg-customwhite p-4 text-sm rounded-lg w-full border ${
+                emailError
+                  ? "border-red-500 text-red-500"
+                  : emailValid
+                  ? "border-green-500 text-green-500"
+                  : "border-inputborder"
+              }`}
             />
+            {emailError ? (
+              <View className="flex flex-row items-center mt-1">
+                <MaterialIcons
+                  name="warning"
+                  size={20}
+                  color="red"
+                  className="mr-2"
+                />
+                <Text className="text-red-500 text-xs">{emailError}</Text>
+              </View>
+            ) : null}
 
             {/* Contraseña */}
-            <Text className="-mb-3 uppercase font-afacad-regular text-base">
+            <Text className="-mb-3 text-typography_2 font-raleway-medium text-base">
               Contraseña
             </Text>
-            <TextInput
-              placeholder="************"
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-              className="bg-[#f2f2f2] border border-inputborder rounded-lg w-full p-2"
-            />
+            <View className="relative">
+              <TextInput
+                placeholder="Introduce tu contraseña"
+                secureTextEntry={secureText}
+                value={password}
+                onChangeText={setPassword}
+                className={`bg-customwhite p-4 text-sm rounded-lg w-full border ${
+                  passwordError
+                    ? "border-red-500 text-red-500"
+                    : passwordValid
+                    ? "border-green-500 text-green-500"
+                    : "border-inputborder"
+                }`}
+              />
+              <TouchableOpacity
+                onPress={() => setSecureText(!secureText)}
+                className="absolute right-2 top-1/2 transform -translate-y-1/2"
+              >
+                <MaterialIcons
+                  name={secureText ? "visibility" : "visibility-off"}
+                  size={24}
+                  color="#666"
+                />
+              </TouchableOpacity>
+            </View>
+            {passwordError ? (
+              <View className="flex flex-row items-center mt-1">
+                <MaterialIcons
+                  name="warning"
+                  size={20}
+                  color="red"
+                  className="mr-2"
+                />
+                <Text className="text-red-500 text-xs">{passwordError}</Text>
+              </View>
+            ) : null}
 
             {/* Confirmar contraseña */}
-            <Text className="-mb-3 uppercase font-afacad-regular text-base">
+            <Text className="-mb-3 text-typography_2 font-raleway-medium text-base">
               Repetir contraseña
             </Text>
-            <TextInput
-              placeholder="************"
-              secureTextEntry
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-              className="bg-[#f2f2f2] border border-inputborder rounded-lg w-full p-2"
-            />
+            <View className="relative">
+              <TextInput
+                placeholder="Repite tu contraseña"
+                secureTextEntry={secureText}
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                className={`bg-customwhite p-4 text-sm rounded-lg w-full border ${
+                  confirmPasswordError
+                    ? "border-red-500 text-red-500"
+                    : confirmPasswordValid
+                    ? "border-green-500 text-green-500"
+                    : "border-inputborder"
+                }`}
+              />
+              <TouchableOpacity
+                onPress={() => setSecureText(!secureText)}
+                className="absolute right-2 top-1/2 transform -translate-y-1/2"
+              >
+                <MaterialIcons
+                  name={secureText ? "visibility" : "visibility-off"}
+                  size={24}
+                  color="#666"
+                />
+              </TouchableOpacity>
+            </View>
+            {confirmPasswordError ? (
+              <View className="flex flex-row items-center mt-1">
+                <MaterialIcons
+                  name="warning"
+                  size={20}
+                  color="red"
+                  className="mr-2"
+                />
+                <Text className="text-red-500 text-xs">
+                  {confirmPasswordError}
+                </Text>
+              </View>
+            ) : null}
 
-            {/* Mostrar mensaje de error si existe */}
+            {/* Requisitos de la contraseña con el ícono */}
+            <View className="flex flex-row items-start mt-2">
+              <MaterialIcons
+                name="info-outline"
+                size={20}
+                color="#222222"
+                className="mr-2"
+              />
+              <Text className="text-sm text-raleway-medium text-typography_2 text-pretty">
+                Requisitos de la contraseña - Mínimo de 8 dígitos, una
+                mayúscula, un número y un símbolo.
+              </Text>
+            </View>
+
+            {/* Mostrar mensaje de error general */}
             {errorMessage ? (
               <Text className="text-red-500 text-base mt-2 text-center">
                 {errorMessage}
               </Text>
             ) : null}
+          </View>
 
-            {/* Botón de registro */}
+          <View className="flex mb-20 gap-2 px-12 w-full">
             <TouchableOpacity
+              className={`${
+                isButtonDisabled ? "bg-gray-300" : "bg-primary"
+              } px-14 py-4 rounded-2xl mt-4`}
               onPress={handleRegister}
-              className="bg-primary px-14 py-4 rounded-2xl mt-4"
+              disabled={isButtonDisabled}
             >
-              <Text className="text-white text-center font-raleway-semibold text-sm">
+              <Text className="text-white text-center font-raleway-semibold text-base">
                 Crear cuenta
               </Text>
             </TouchableOpacity>
-          </View>
-
-          {/* Link para iniciar sesión */}
-          <Text className="font-raleway-regular text-base mb-14">
-            ¿Ya tienes cuenta?{" "}
             <Text
-              className="font-raleway-bold text-typography_2"
+              className="text-center text-xs text-typography_2 font-raleway-medium"
               onPress={() => navigation.navigate("Login")}
             >
-              ¡Identifícate!
+              ¿Ya tienes cuenta? Inicia sesión.
             </Text>
-          </Text>
+          </View>
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
