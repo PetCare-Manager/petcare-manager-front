c1a7746f779ab09601d1c77dde139b639e4234f9
Carol-88 <gralleta@gmail.com>
2024-12-18 18:41:04 +0100

    add Screen in screen components

---
+++ components/ChangePassScreen.tsx
@@ -0,0 +1,109 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import axios from "axios";
+import React, { useState } from "react";
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SvgIconsComponent } from "./commons/SvgIconsComponent";
+
+type RootStackParamList = {
+  Home: undefined;
+  Register: undefined;
+  EmailRegister: undefined;
+  Login: undefined;
+  NotRememberPassScreen: undefined;
+};
+
+type NotRememberPassScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  "Home"
+>;
+
+export const ChangePassScreen: React.FC<NotRememberPassScreenProps> = ({
+  navigation,
+}) => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleResetPassword = async () => {
+    if (!password || !confirmPassword) {
+      setError("Ambos campos son obligatorios");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setError("Las contraseñas no coinciden");
+      return;
+    }
+    setError("");
+
+    try {
+      const response = await axios.post(
+        "https://api.example.com/reset-password",
+        {
+          password,
+        }
+      );
+
+      Alert.alert("Contraseña reestablecida con éxito");
+      navigation.navigate("Login");
+    } catch (error: any) {
+      if (error.response) {
+        setError(
+          error.response.data.message || "Error al reestablecer la contraseña"
+        );
+      } else {
+        setError("Error de conexión. Intente nuevamente.");
+      }
+    }
+  };
+
+  return (
+    <View className="flex flex-col justify-between items-center h-full">
+      <SvgIconsComponent
+        containerClass="w-56 h-44 z-10 mt-[120px] items-center"
+        type="logo1"
+      />
+
+      <View className="items-center -mt-30 px-6">
+        <Text className="font-afacad-semibold text-[40px] lg:text-5xl text-center text-typography">
+          Recuperar contraseña
+        </Text>
+        <View className="flex gap-4 mt-4 mb-6 w-full px-12">
+          <Text className="-mb-3 uppercase font-afacad-regular text-base">
+            Contraseña nueva
+          </Text>
+          <TextInput
+            placeholder="Ingresa tu nueva contraseña"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            className="bg-[#f2f2f2] border border-inputborder rounded-lg w-full p-2"
+          />
+
+          <Text className="-mb-3 uppercase font-afacad-regular text-base">
+            Repetir contraseña
+          </Text>
+          <TextInput
+            placeholder="Repite tu nueva contraseña"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+            className="bg-[#f2f2f2] border border-inputborder rounded-lg w-full p-2"
+          />
+
+          {error ? (
+            <Text className="text-red-500 text-center mt-2">{error}</Text>
+          ) : null}
+        </View>
+
+        <TouchableOpacity
+          onPress={handleResetPassword}
+          className="bg-primary px-14 py-4 rounded-2xl mt-4"
+        >
+          <Text className="text-white text-center font-raleway-semibold text-base">
+            Reestablecer contraseña
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
---
+++ components/CreateEventButton.tsx
@@ -0,0 +1,38 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+type RootStackParamList = {
+  FormEvent: undefined;
+};
+// Definir el tipo de navegación para CreateEvent
+type FormEventProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList, "FormEvent">;
+};
+
+export const CreateEvent: React.FC<FormEventProps> = ({ navigation }) => {
+  return (
+    <View className="flex flex-row items-center bg-white rounded-lg p-4">
+      <View className="container flex">
+        <View className="flex-row justify-center items-center rounded-lg shadow p-4">
+          <View className="flex bg-primary rounded-lg w-12 h-12 items-center justify-center mr-4">
+            <MaterialIcons name="alarm" size={24} color="#fff" />
+          </View>
+          {/* Texto de añadir evento */}
+          <Text className="text-lg text-primary font-raleway-semibold flex-1">
+            Añadir Evento
+          </Text>
+
+          {/* Botón de añadir mascota */}
+          <TouchableOpacity
+            onPress={() => navigation.navigate("FormEvent")}
+            className="bg-pink-200 rounded-full p-2"
+          >
+            <MaterialIcons name="add" size={24} color="#c04b71" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
