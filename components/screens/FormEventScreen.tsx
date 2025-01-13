ef45e7e39a6dc38eb8929e65412491f76f0faaf9
Carol-88 <gralleta@gmail.com>
2025-01-13 13:22:53 +0100

    work in ui

--- components/screens/FormEventScreen.tsx
+++ components/screens/FormEventScreen.tsx
@@ -1,6 +1,6 @@
 import { Event } from "@/components/Event";
 import React, { useState } from "react";
-import { Button, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export const FormEvent: React.FC = () => {
   const [title, setTitle] = useState("");
@@ -13,26 +13,42 @@ export const FormEvent: React.FC = () => {
   };
 
   return (
-    <View className="p-4">
+    <View className="p-4 font-raleway-regular">
+      <Text className="text-sm text-typography font-raleway-semibold uppercase mb-2">
+        Titulo del evento
+      </Text>
       <TextInput
-        placeholder="Título del evento"
+        placeholder="Ej: Visita al veterinario"
         value={title}
         onChangeText={setTitle}
-        className="border p-2 mb-4"
+        className="bg-customwhite border border-inputborder font-raleway-light rounded-lg p-4 mb-4"
       />
+      <Text className="text-sm text-typography font-raleway-semibold uppercase mb-2">
+        Fecha del evento
+      </Text>
       <TextInput
-        placeholder="Fecha (YYYY-MM-DD)"
+        placeholder="DD/MM/AAAA"
         value={date}
         onChangeText={setDate}
-        className="border p-2 mb-4"
+        className="bg-customwhite border border-inputborder font-raleway-light rounded-lg p-4 mb-4"
       />
+      <Text className="text-sm text-typography font-raleway-semibold uppercase mb-2">
+        Hora del evento
+      </Text>
       <TextInput
-        placeholder="Hora (HH:mm)"
+        placeholder="hh:mm"
         value={time}
         onChangeText={setTime}
-        className="border p-2 mb-4"
+        className="bg-customwhite border border-inputborder font-raleway-light rounded-lg p-4 mb-4"
       />
-      <Button title="Añadir perro" onPress={handleAddDogIcon} />
+
+      <View className="flex mb-20 gap-2 px-12 w-full">
+        <TouchableOpacity onPress={handleAddDogIcon}>
+          <Text className="text-white bg-primary px-14 py-4 rounded-2xl mt-4 text-center font-raleway-semibold text-base">
+            Crear evento
+          </Text>
+        </TouchableOpacity>
+      </View>
       <View className="my-4">
         {/* Si no hay datos, pasamos los campos vacíos y se mostrarán valores por defecto */}
         <Event
