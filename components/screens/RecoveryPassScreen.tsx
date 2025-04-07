a9750b80e9dbec31857bd7fa99082f92f729f2e6
Salvador De la Tijera <salvagerar@gmail.com>
2025-04-07 11:55:15 -0600

    prueba push

--- components/screens/RecoveryPassScreen.tsx
+++ components/screens/RecoveryPassScreen.tsx
@@ -9,7 +9,7 @@ import {
 import { SvgIconsComponent } from "../commons/SvgIconsComponent";
 
 export const RecoveryPassScreen = () => {
-{/**efiowfewojefojeojeowfjeowfjeowjoepwfjeopwfjewopfjewofjewopjewopfjewopfejwopfj */}
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>{/**  para que el contenido pueda crecer y ocupar todo el espacio disponible*/}
       <View className="flex flex-col justify-between items-center h-full">
