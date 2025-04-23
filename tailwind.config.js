79e0e79e800784c32983f954e293e056e026c773
Carolina Romero Cotán <114145394+Carol-88@users.noreply.github.com>
2025-04-23 13:15:03 +0200

    Revert "5 create petcard"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      fontFamily: {
-        "afacad-regular": ["AfacadFlux-Regular"],
-        "afacad-semibold": ["AfacadFlux-SemiBold"],
-        "raleway-extralight": ["Raleway-ExtraLight"],
-        "raleway-light": ["Raleway-Light"],
-        "raleway-regular": ["Raleway-Regular"],
-        "raleway-medium": ["Raleway-Medium"],
-        "raleway-semibold": ["Raleway-SemiBold"],
-        "raleway-bold": ["Raleway-Bold"],
-        "raleway-black": ["Raleway-Black"],
-      },
-      colors: {
-        primary: "#d75c7b",
-        secondary: "#72C3E0",
-        typography: "#4D5C61",
-        typography_2: "#292929",
-        inputborder: "#A6A6A6",
-        customyellow: "#EEE07D",
-        customwhite: "#F4F7F7",
-        customgrey: "F3F6F6",
-        statusred: "#F94E4E",
-        statusyellow: "#e5c23a",
-        statusgreen: "#007E4E",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-  darkMode: false,
-};
+}
