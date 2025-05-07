afc34baa1deb1e7a921adb25887643c1c19861a9
Carol-88 <gralleta@gmail.com>
2025-05-07 18:56:33 +0200

    change hard values from tailwind values

--- components/commons/LogoBig.tsx
+++ components/commons/LogoBig.tsx
@@ -4,12 +4,12 @@ import { SvgIconsComponent } from "./SvgIconsComponent";
 
 export const LogoBig = () => {
   return (
-    <View className="flex flex-col items-center">
+    <View className="flex flex-col items-center text-">
       <SvgIconsComponent
-        containerClass="w-56 h-44 z-10 mt-[120px] items-center"
+        containerClass="w-56 h-44 z-10 mt-32 items-center"
         type="logo1"
       />
-      <Text className="font-afacad-semibold text-[40px] lg:text-5xl text-center text-typography">
+      <Text className="font-afacad-semibold text-4xl lg:text-5xl text-center text-typography">
         PetCare Manager
       </Text>
     </View>
